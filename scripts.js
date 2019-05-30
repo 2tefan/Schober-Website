ebe14bf8eb3a07a611344de7c5493dd9bba37729
Stefan <201310.stefan@gmail.com>
2019-05-30 02:06:44 +0200

    Redesign

--- scripts.js
+++ scripts.js
@@ -2,3 +2,14 @@ $(document).ready(function () {
     $('#header').load("header.html");
     $('#footer').load("footer.html");
 });
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+        $('navbar').addClass('fixed-header');
+        $('navbar div').addClass('visible-title');
+    }
+    else {
+        $('navbar').removeClass('fixed-header');
+        $('navbar div').removeClass('visible-title');
+    }
+});
