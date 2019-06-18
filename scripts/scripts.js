7f517728cff264f368ed04c15ead443d71f3ad5c
Stefan <201310.stefan@gmail.com>
2019-06-18 19:06:15 +0200

    Responsive Navbar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,36 +1,34 @@
 $(document).ready(function () {
     $('#header').load("../../html/elements/header.html");
-    $('#navbar').load("../../html/elements/navbar.html");
+    $('#myNavbar').load("../../html/elements/navbar.html");
     $('#games').load("../../html/elements/games.html");
     $('#footer').load("../../html/elements/footer.html");
 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 300) { 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
             $('#returnToTop').addClass('show');
-        } else { 
+        } else {
             $('#returnToTop').removeClass('show');
-        } 
+        }
     });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
     // When the user scrolls the page, execute myFunction 
     window.onscroll = function () {
-        myFunction()
-    };
-
-    // Get the navbar
-    var navbar = document.getElementById("navbar");
-
-    // Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
-
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
-        if (window.pageYOffset >= 124) {
-            navbar.classList.add("sticky")
+        if (window.pageYOffset >= $('#headline').outerHeight(true)) {
+            $('#myNavbar').addClass("sticky");
         } else {
-            navbar.classList.remove("sticky");
+            $('#myNavbar').removeClass("sticky");
         }
-    }
+    };
 })
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function navbar() {
+    if ($('#myNavbar').hasClass("responsive")) {
+        $('#myNavbar').removeClass("responsive");
+    } else {
+        $('#myNavbar').addClass("responsive");
+    }
+}
