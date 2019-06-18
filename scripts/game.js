8e9280a5e8cf10c65a1609118b49924dc1857576
Stefan <201310.stefan@gmail.com>
2019-06-18 14:59:16 +0200

    Added player

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,21 @@
 function startGame() {
-    window.setInterval(redraw,20);
+    resizedWindow();
+    window.setInterval(redraw, 20);
+}
+
+function resizedWindow() {
+    myGameArea.start();
+    resetGame();
+}
+
+function resetGame() {
+    player = new playerObject(30, 30, "red", 10, 120);
+}
+
+function redraw() {
+    myGameArea.clear();
+    myGameArea.draw();
+    player.redraw();
 }
 
 var myGameArea = {
@@ -9,19 +25,29 @@ var myGameArea = {
         this.canvas.height = window.innerHeight;
         this.context = this.canvas.getContext("2d");
 
-        this.context.moveTo(0, 0);
-        this.context.lineTo(this.canvas.width, this.canvas.height);
-        this.context.stroke();
-
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     },
     clear: function () {
-        this.canvas.getContext("2d").clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    draw: function () {
+        this.context.moveTo(0, 0);
+        this.context.lineTo(this.canvas.width, this.canvas.height);
+        this.context.stroke();
+        this.context.moveTo(this.canvas.width, 0);
+        this.context.lineTo(0, this.canvas.height);
+        this.context.stroke();
     }
 }
 
-function redraw()
-{
-    myGameArea.clear();
-    myGameArea.start();
+function playerObject(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.redraw = function () {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
