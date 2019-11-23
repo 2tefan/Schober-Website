8bbb6e68f9afc998c872ef74a3840c42e7f5e853
Stefan <201310.stefan@gmail.com>
2019-11-23 23:26:20 +0100

    Fixed a bug when switching between mobile and desktop website

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,21 @@
 function navbarAnimations() {
   var navbar = document.getElementById("navbarLinks");
 
-  if (navbar.classList.contains("navbarAniOpen")) {
-    navbar.classList.remove("navbarAniOpen");
-    navbar.classList.add("navbarAniClose");
-  } else {
-    navbar.classList.add("navbarAniOpen");
-    navbar.classList.remove("navbarAniClose");
+  if (window.screen.width < 500) {
+    if (navbar.classList.contains("navbarAniOpen")) {
+      navbar.classList.remove("navbarAniOpen");
+      navbar.classList.add("navbarAniClose");
+    } else {
+      navbar.classList.add("navbarAniOpen");
+      navbar.classList.remove("navbarAniClose");
+    }
   }
 }
+window.onresize = function(event) {
+  var navbar = document.getElementById("navbarLinks");
+  
+  if (window.screen.width >= 500) {
+    navbar.classList.remove("navbarAniOpen");
+    navbar.classList.remove("navbarAniClose");
+  }
+};
