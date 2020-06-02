5898321a9db112fa44d1a169ae5b08a52274b596
2tefan <2tefan@protonmail.com>
2020-06-02 18:36:49 +0200

    Optimized SCSS

---
+++ scripts/downloads.js
@@ -0,0 +1,30 @@
+// Looks if the animation is finished
+function whichAnimationEvent() {
+  var t,
+    el = document.createElement("fakeelement");
+
+  var animations = {
+    animation: "animationend",
+    OAnimation: "oAnimationEnd",
+    MozAnimation: "animationend",
+    WebkitAnimation: "webkitAnimationEnd",
+  };
+
+  for (t in animations) {
+    if (el.style[t] !== undefined) {
+      return animations[t];
+    }
+  }
+}
+
+var animationEvent = whichAnimationEvent();
+
+jQuery(document).ready(function ($) {
+  //When a image with the class aniIMG is clicked, it spins and then it gets resetted.
+  $(".aniIMG").on("click", function (e) {
+    $(this).addClass("rotateAni");
+    $(this).one(animationEvent, function (event) {
+      $(this).removeClass("rotateAni");
+    });
+  });
+});
